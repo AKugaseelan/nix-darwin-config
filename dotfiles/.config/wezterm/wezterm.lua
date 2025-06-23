f660e1cae40f24597f8edd6c253c5ec23f2bdd77
Athiraiyan Kugaseelan <athiraiyan.kugaseelan@outlook.com>
2025-06-23 19:07:19 +0200

    Fixed double entry for fancy tab bar

--- dotfiles/.config/wezterm/wezterm.lua
+++ dotfiles/.config/wezterm/wezterm.lua
@@ -9,7 +9,6 @@ wezterm.plugin
 config = {
     automatically_reload_config = true,
     use_fancy_tab_bar = false,
-    use_fancy_tab_bar = true,
     hide_tab_bar_if_only_one_tab = true,
     window_close_confirmation = "NeverPrompt",
     window_decorations = "RESIZE",
