5b2455675cfa37e52764ad71ef8e04577aa4942c
Athi Boog <athiraiyan.kugaseelan@outlook.com>
2026-05-22 01:25:17 +0200

    updated commit message for nix update alias

--- home.nix
+++ home.nix
@@ -43,7 +43,7 @@
     shellAliases = {
       drs = "sudo darwin-rebuild switch --flake ~/nix-darwin-config#MacBook-Pro";
       nfu = "nix flake update";
-      nua = "nix flake update --flake ~/nix-darwin-config && sudo darwin-rebuild switch --flake ~/nix-darwin-config#MacBook-Pro && git -C ~/nix-darwin-config add flake.lock && git -C ~/nix-darwin-config commit -m 'chore: ran nix flake update' && git -C ~/nix-darwin-config push";
+      nua = "nix flake update --flake ~/nix-darwin-config && sudo darwin-rebuild switch --flake ~/nix-darwin-config#MacBook-Pro && git -C ~/nix-darwin-config add flake.lock && git -C ~/nix-darwin-config commit -m 'chore: Updated system fully using flake update and darwin rebuild && git -C ~/nix-darwin-config push";
       cd = "z";
       cat = "bat";
       lg = "lazygit";
